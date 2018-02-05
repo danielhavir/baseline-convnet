97437174d67dfc7a645a0f88cd6939a2c71539bd
Daniel Havir <havir.daniel@gmail.com>
2018-02-05 23:30:39 +0100

    Minor updates

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import tensorflow_model.train as tf_train
-from pytorch_model.train import BaselineConvnet
+#from pytorch_model.train import BaselineConvnet
 
 LABEL_NAMES = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 epochs = 25
-batch_size = 128
+batch_size = 1024
 keep_probability = 0.5
-learning_rate = 3e-4
+learning_rate = 3e-1
 
 # TensorFlow
-# tf_train.run(epochs, batch_size, learning_rate, keep_probability)
+tf_train.run(epochs, batch_size, learning_rate, keep_probability)
 
 # PyTorch
-baseline_convnet = BaselineConvnet(batch_size, keep_probability, learning_rate)
-baseline_convnet.run(epochs, LABEL_NAMES)
+#baseline_convnet = BaselineConvnet(batch_size, keep_probability, learning_rate)
+#print(baseline_convnet.net)
+#baseline_convnet.run(epochs, LABEL_NAMES)
--- pytorch_model/train.py
+++ pytorch_model/train.py
@@ -23,11 +23,13 @@ class BaselineConvnet:
 		self.trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
 		self.testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=self.transform)
-		self.testloader = torch.utils.data.DataLoader(self.testset, batch_size=4, shuffle=False, num_workers=2)
+		self.testloader = torch.utils.data.DataLoader(self.testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
 		self.net = BaselineModel(10, keep_probability)
 		if self.use_gpu:
 			self.net = self.net.cuda()
+		if torch.cuda.device_count() > 1:
+			self.net = nn.DataParallel(self.net)
 
 		self.criterion = nn.CrossEntropyLoss()
 		self.optimizer = optim.Adam(self.net.parameters(), lr=learning_rate)
